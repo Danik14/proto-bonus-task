1c0e9ffd188db2af7cf9d85a61430b2c31d258fb
Danik <chapagana@gmail.com>
2023-04-24 23:08:16 +0600

    first commit

--- proto.go
+++ proto.go
@@ -8,26 +8,22 @@ import (
 )
 
 func main() {
-	// Create a new PhoneNumber message.
 	phone := &gen.PhoneNumber{
 		Number: "123-456-7890",
 		Type:   gen.PhoneType_MOBILE,
 	}
 
-	// Create a new Person message.
 	person := &gen.Person{
-		Name:        "John Doe",
-		Id:          123,
-		Email:       "johndoe@example.com",
+		Name:        "Ryan Gosling",
+		Id:          1,
+		Email:       "ryan@gosling.com",
 		Phones:      []*gen.PhoneNumber{phone},
-		LastUpdated: &timestamp.Timestamp{Seconds: 1619410882},
+		LastUpdated: &timestamp.Timestamp{Seconds: 12345},
 	}
 
-	// Create a new AddressBook message.
 	addressBook := &gen.AddressBook{
 		People: []*gen.Person{person},
 	}
 
-	// Print the contents of the AddressBook message.
 	fmt.Println(addressBook)
 }
