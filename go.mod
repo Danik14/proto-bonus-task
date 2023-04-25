28d484d9f7d3256d84e76258a424e479d36737ed
Danik <chapagana@gmail.com>
2023-04-25 10:43:09 +0600

    Done

--- go.mod
+++ go.mod
@@ -7,4 +7,9 @@ require (
 	google.golang.org/protobuf v1.28.1
 )
 
-require github.com/google/go-cmp v0.5.9 // indirect
+require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+
+require (
+	github.com/google/go-cmp v0.5.9 // indirect
+	go.starlark.net v0.0.0-20230302034142-4b1e35fe2254
+)
--- proto.go
+++ proto.go
@@ -4,26 +4,19 @@ import (
 	"fmt"
 	"test/gen"
 
-	"github.com/golang/protobuf/ptypes/timestamp"
+	"google.golang.org/protobuf/proto"
 )
 
 func main() {
-	phone := &gen.PhoneNumber{
-		Number: "123-456-7890",
-		Type:   gen.PhoneType_MOBILE,
+	// Binary encoded phone number
+	binData := []byte{10, 45, 10, 8, 74, 111, 104, 110, 32, 68, 111, 101, 16, 210, 9, 26, 16, 106, 100, 111, 101, 64, 101, 120, 97, 109, 112, 108, 101, 46, 99, 111, 109, 34, 12, 10, 8, 53, 53, 53, 45, 52, 51, 50, 49, 16, 3}
+
+	// Unmarshal the binary data into a AddressBook message
+	person := &gen.AddressBook{}
+	err := proto.Unmarshal(binData, person)
+	if err != nil {
+		panic(err)
 	}
 
-	person := &gen.Person{
-		Name:        "Ryan Gosling",
-		Id:          1,
-		Email:       "ryan@gosling.com",
-		Phones:      []*gen.PhoneNumber{phone},
-		LastUpdated: &timestamp.Timestamp{Seconds: 12345},
-	}
-
-	addressBook := &gen.AddressBook{
-		People: []*gen.Person{person},
-	}
-
-	fmt.Println(addressBook)
+	fmt.Println(person)
 }
